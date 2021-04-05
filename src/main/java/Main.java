73f47dabfcbca2065f1052be5856d6e1bb3a4464
Péter Hegedűs <hpeti0912@gmail.com>
2021-04-05 10:46:17 +0200

    Added basic logging, markers (ThreadContext for later usage)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import org.apache.logging.log4j.*;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger();
+    private static final Marker KIJELENT = MarkerManager.getMarker("KIJELENT");
+    private static final Marker KÉRELEM = MarkerManager.getMarker("KÉRELEM");
     public static void main(String[] args) {
+        int iterationCount = 2;
+        if(args.length == 1){
+            iterationCount = Integer.parseInt(args[0]);
+        }
+        for (int i = 1; i <= iterationCount; i++){
+            ThreadContext.put("iterationNumber", String.valueOf(i));
+
+            logger.trace(KIJELENT, "Erdőn-mezőn nyitott szemmel,");
+            logger.debug(KIJELENT, "sok virágra lel az ember.");
+            logger.info(KIJELENT,"Én most őket megöntözöm,");
+            logger.warn("a piros tojást megköszönöm!");
+            logger.error(KÉRELEM, "Szabad-e locsolni?");
+
+            ThreadContext.clearAll();
+        }
 
     }
 }
