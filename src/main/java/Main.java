efd4bca306073cd223006eab1cbf8a6bfce756e9
Péter Hegedűs <hpeti0912@gmail.com>
2021-04-05 14:35:04 +0200

    Added Thread.sleep() and exception throwing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,17 @@
 import org.apache.logging.log4j.*;
 
 public class Main {
+
     private static final Logger logger = LogManager.getLogger();
     private static final Marker KIJELENT = MarkerManager.getMarker("KIJELENT");
     private static final Marker KÉRELEM = MarkerManager.getMarker("KÉRELEM");
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         int iterationCount = 2;
         if(args.length == 1){
             iterationCount = Integer.parseInt(args[0]);
         }
+
         for (int i = 1; i <= iterationCount; i++){
             ThreadContext.put("iterationNumber", String.valueOf(i));
 
@@ -19,7 +22,7 @@ public class Main {
             logger.error(KÉRELEM, "Szabad-e locsolni?");
 
             ThreadContext.clearAll();
+            Thread.sleep(1000);
         }
-
     }
 }
